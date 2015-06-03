d8e8325441f39a76a23076247e1fb2a0dac18231
Georg Ringer <georg.ringer@gmail.com>
2015-06-03 09:52:46 +0200

    [TASK] Metadata as readonly

--- Configuration/TCA/Overrides/tx_news_domain_model_news.php
+++ Configuration/TCA/Overrides/tx_news_domain_model_news.php
@@ -9,8 +9,11 @@ $fields = array(
 		'label' => 'LLL:EXT:news_importicsxml/Resources/Private/Language/locallang.xlf:tx_news_domain_model_news.news_import_data',
 		'config' => array(
 			'type' => 'text',
+			'cols' => 60,
+			'rows' => 50,
+			'readOnly' => TRUE
 		)
-	),
+	)
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tx_news_domain_model_news', $fields);
