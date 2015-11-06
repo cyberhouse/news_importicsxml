c8512c800655d3737fe47c3def3ce5c9500529f0
Georg Ringer <github@ringerge.org>
2015-11-06 08:13:35 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' =>
 		array(
 			'depends' => array(
-				'typo3' => '6.2.0-6.2.99',
+				'typo3' => '6.2.0-7.6.99',
 				'news' => '3.2.0-3.3.99'
 			),
 			'conflicts' => array(),
