07509f951b1c5366b0f9ff2a490e54ae9d21afee
Aristeidis <aristeidis.karavas@ifabrik.de>
2021-03-29 17:56:34 +0200

    TYPO3 10-11 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF['news_importicsxml'] = [
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Import of ICS & XML to EXT:news',
     'description' => 'Versatile news import from ICS + XML (local files or remote URLs) including images and category mapping',
     'category' => 'backend',
